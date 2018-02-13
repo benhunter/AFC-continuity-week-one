c9a4a9cbbe1668c1b2691ef03dac40eaa509bd1e
Jay Batavia <jay.batavia@galvanize.com>
2018-02-13 10:34:39 -0700

    Flesh out lightsaber class

--- src/main/java/com/galvanize/LightSaber.java
+++ src/main/java/com/galvanize/LightSaber.java
@@ -2,11 +2,55 @@ package com.galvanize;
 
 public class LightSaber {
 
-    private float charge = 0.0f;
-    private long odometer = 0;
-    private String =
-    private String color;
+    private static float EFFICIENCY = 10.0f;
 
+    private float charge = 100.0f;
+    private long jediSerialNumber;
+    private String color = "green";
 
+    public LightSaber(long jediSerialNumber){
+        this.jediSerialNumber = jediSerialNumber;
+    }
 
+    public void setCharge(float charge) {
+        this.charge = charge;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getCharge() {
+        return charge;
+
+    }
+
+    public long getJediSerialNumber() {
+        return jediSerialNumber;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void use(float minutes) {
+        charge -= (EFFICIENCY / 60.0f) * minutes;
+    }
+
+    public float getRemainingMinutes() {
+        return ((charge/EFFICIENCY)*30);
+    }
+
+    public void recharge(){
+        charge = 100.0f;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "LightSaber{" +
+//                "charge=" + charge +
+//                ", jediSerialNumber=" + jediSerialNumber +
+//                ", color='" + color + '\'' +
+//                '}';
+//    }
 }
