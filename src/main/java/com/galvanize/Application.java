ae00c2dd3e82a0fa3992318dd4def1efcad4ea42
Jay Batavia <jay.batavia@galvanize.com>
2018-02-14 10:56:01 -0700

    Removed examples from main due to jacoco

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -3,24 +3,6 @@ package com.galvanize;
 public class Application {
 
     public static void main(String... args) {
-        LightSaber myLightSaber = new LightSaber(197719801983L);
 
-        //Check the state
-        System.out.println("New: " + myLightSaber);
-
-        //Use lightsaber for two hours
-        myLightSaber.use(120.0f);
-
-        //Check the state
-        System.out.println("Used: " + myLightSaber);
-
-        //Check how much longer you can use it
-        System.out.println("Remaining minutes: " + myLightSaber.getRemainingMinutes());
-
-        //Change color
-        myLightSaber.setColor("blue");
-
-        //Check the state
-        System.out.println("Color change: " + myLightSaber);
     }
 }
